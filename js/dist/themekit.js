5689ddc24dfb0a8cece1c02f324dbbbbfc16a512
Bjorn Breckenridge <bjornbreck@gmail.com>
2018-03-13 21:13:48 -0600

    fun things

--- js/dist/themekit.js
+++ js/dist/themekit.js
@@ -135,6 +135,46 @@
 	    sticky: true,
 	    interactive: true
 	  });
+
+	  var colors = void 0;
+	  var color = 0;
+
+	  (0, _jquery2.default)('.color-text').click(function (e) {
+	    e.preventDefault();
+
+	    if (colors < 6) {
+	      colors++;
+	    } else if (colors < 6) {} else {
+	      colors = 0;
+	    }
+
+	    // console.log(colors);
+	    // console.log(color);
+
+	    switch (colors) {
+	      case 0:
+	        color = '#7a5650';
+	        break;
+	      case 1:
+	        color = '#435454';
+	        break;
+	      case 2:
+	        color = '#1d6b8b';
+	        break;
+	      case 3:
+	        color = '#f2a66f';
+	        break;
+	      case 4:
+	        color = '#e9683d';
+	        break;
+	      case 5:
+	        color = '#db3b29';
+	        break;
+	    }
+	    (0, _jquery2.default)(this).css('color', color);
+	    // $(this).children().first().css('color', color);
+	    // $(this).children().last().css('color', color);
+	  });
 	});
 
 /***/ }),
--- js/src/theme.js
+++ js/src/theme.js
@@ -79,5 +79,50 @@ $(document).ready(function () {
     trigger: 'click',
     sticky: true,
     interactive: true
+  });
+
+  let colors;
+  let color = 0;
+  
+  $('.color-text').click(function(e) {
+    e.preventDefault();
+
+    if (colors < 6) {
+      colors++;
+    } else if (colors < 6) {
+
+    }
+    else {
+      colors = 0;
+    }
+
+    // console.log(colors);
+    // console.log(color);
+
+    switch(colors) {
+      case 0:
+        color = '#7a5650';
+        break;
+      case 1:
+        color = '#435454';
+        break;
+      case 2:
+        color = '#1d6b8b';
+        break;
+      case 3:
+        color = '#f2a66f';
+        break;
+      case 4:
+        color = '#e9683d';
+        break;
+      case 5:
+        color = '#db3b29';
+        break;
+    }
+    $(this).css('color', color);
+    // $(this).children().first().css('color', color);
+    // $(this).children().last().css('color', color);
   })
+
+
 });
